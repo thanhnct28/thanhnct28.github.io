fbf32046d2680454432a8fb754440c41185c8ce3
NGUYEN CHI TAN THANH <70990332+thanhnct28@users.noreply.github.com>
2021-04-05 16:32:35 +0700

    Create student.cs

---
+++ student.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppOOP
+{
+    class Student
+    {
+        // 1. Định nghĩa trường Name:
+        private string Name;
+
+        // 2. Định nghĩa thuộc tính StudentName từ trường Name
+        public string StudentName
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+
+        // 3. Định nghĩa phương thức khởi tạo Student(string stName)
+        public Student(string stName)
+        {
+            Name = stName;
+        }
+
+        public Student()
+        {
+        }
+
+
+
+        // 4. Định nghĩa phương thức Talk() giới thiệu
+        public void Talk()
+        {
+            MessageBox.Show("Thanh");
+        }
+
+        // tên của sinh viên thông qua MessageBox
+    }
+}
